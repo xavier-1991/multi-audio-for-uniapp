5317ba35e14efa75530b1238d4e5987197392805
zhangxd <895163024@qq.com>
2019-06-28 14:21:23 +0800

    项目构建及完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,65 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view style="padding:0 30upx;">
+    <view v-for="(item,index) in audioList"
+          :key="index">
+      <multi-audio :audioId="'a'+index"
+                   :audioData="item"></multi-audio>
+    </view>
+    <view v-for="(item,index) in audioList"
+          :key="index">
+      <multi-audio :audioId="'a'+index"
+                   :audioData="item" deleteBtn="true" @deleteAudio="delAudio"></multi-audio>
+    </view>
+  </view>
+
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import multiAudio from "../../components/multi-audio/multi-audio";
+export default {
+  data() {
+    return {
+      audioList: [
+        {
+          duration: 3,
+          asrc:
+            "https://fapptest.chinayzyx.com/upload/20190627/4898aeac-76e9-4bfe-adf1-98334fabdd97.aac"
+        },
+        {
+          duration: 4,
+          asrc:
+            "https://fapptest.chinayzyx.com/upload/20190627/a0e4afbf-0d25-4f7b-ba2a-a94d8be86966.aac"
+        }
+      ]
+    };
+  },
+    components: {
+    "multi-audio":multiAudio
+  },
+  onLoad() {},
+  methods: {
+      delAudio(e){
+          //删除音频
+          console.log(e)
+      }
+  }
+};
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+.content {
+  text-align: center;
+  height: 400upx;
+}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36upx;
+  color: #8f8f94;
+}
 </style>
